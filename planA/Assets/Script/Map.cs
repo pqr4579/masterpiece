32b8e933dd16620284e8e30fa003218f936af297
DESKTOP-GBR3NJB\User4579 <dlwjdwls6504@gmail.com>
2019-02-27 00:55:56 +0900

    맵 생성, 타워 스크립트 , 랜덤 배치 메소드 추가, 타일 프리팹

---
+++ planA/Assets/Script/Map.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public int sizeX = 1;
+    public int sizeY = 1;
+    public List<Tile> mapsize = new List<Tile>();
+    public Tile tile;
+    public Transform _Map;
+    // Use this for initialization
+    private void makingMap()
+    {
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+       
+                Tile _Tile = Instantiate(tile) as Tile;
+                _Tile.transform.localPosition = new Vector3(11 * j, 0, 11*i);
+                _Tile.transform.parent = _Map;
+                mapsize.Add(_Tile);
+
+            }
+        }
+    }
+
+    public void RandomTower()
+    {
+
+    }
+    private void Awake()
+    {
+    }
+    void Start ()
+    {
+        makingMap();
+	}	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ planA/Assets/Script/Tile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public Vector2 point;
+    public int build_able;
+
+    private void Awake()
+    {
+
+    }
+    // Use this for initialization
+    void Start () {
+        point = this.gameObject.transform.position;
+        build_able = 0;//건설 가능 지역
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
